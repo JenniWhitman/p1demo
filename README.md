7fd49e15e5998068bb75fc65affbdaa77d12c473
Jenni Whitman <jenni@hbs.edu>
2016-03-03 14:46:05 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Jenni Whitman
+
+## Project URL:
+<http://p1.dwa15-jenni.me>  
+
+## Description:
+My demo of P1 for my section.  Enjoy!
+
+
+This app brought to you by Two Fingers <https://www.youtube.com/watch?v=cxk78XBc--w>
+
+
+## Outside code:
+
+Bootstrap: <https://getbootstrap.com>  
---
+++ index.php
@@ -0,0 +1,143 @@
+<!-- Jenni's P1 demo
+-->
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<!-- Set the viewport so this responsive site displays correctly on mobile devices -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>P1 | Jenni Whitman</title>
+	<meta name="description" content="Jenni Whitman P1 Demo">
+	<meta name="author" content="Jenni Whitman">
+
+
+	<!-- add style - fabulous! -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="/css/styles.css" rel = "stylesheet" >
+	<script src="js/jquery-1.6.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/init.js"></script>
+
+	<!-- fonts are cool -->
+	<link href='http://fonts.googleapis.com/css?family=Josefin+Sans|Raleway' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+	<!-- navbar -->
+	<div class="navbar navbar-inverse navbar-fixed-top">
+	  <div class="navbar-inner">
+	  	<div class="container">
+		    <a class="brand scroll" href="#about" class="scroll">Jenni Whitman</a>
+		    <ul class="nav">
+		      <li><a href="#about" class="scroll">P1</a></li>
+		      <li><a href="#about" class="scroll">About</a></li>
+		      <li><a href="#links" class="scroll">Links</a></li>
+		      <li><a href="#contact" class="scroll">Contact</a></li>
+		    </ul>
+		</div>
+	  </div>
+	</div> 
+
+	<!-- P1 Demo Section -->
+	<section id="about" data-speed="6" data-type="background">
+
+	    <div class="hero-unit">
+	    	<!-- About Anchor -->
+    		<a name="about"></a>
+	    	
+		    <!-- About Me Section -->
+		    <div class="row-fluid">  <!-- open row -->
+		    	<div class="span4 text-center">
+		    		<img src="images/ta-jenni.png">
+		    		<img src="images/jenni_whitman.png">
+		    	</div>
+		    <div class="span8">
+	    		<h2>Welcome to my P1 demo</h2>
+	    		<p>I've created this Demo to give my section an understanding of my finished product.</p>
+	    		<br>
+	    		<h2>In 2+ sentences describe who you are and what you do</h2>
+	    		<p>I'm a geek, mom, gamer, IT professional, student, and teaching assistant.  I love computers.  I love to code.  I love to learn and I love to share knowledge.</p>
+	    		<br>
+	    		<h2>Why are you <strike>taking</strike> TAing this course?</h2>
+	    		<p>Because I loved this course.  I made something here.  I learned something here.  And it wasn't painful.  I'd love to share that experience and learn more.  Plus, in my opinion, Susan is pretty awesome.</p>
+	    		<br>
+	    		<h2>What previous experience do you have with programming?</h2>
+	    		<p>I started on Java around 11 years ago.  I've done coursework in: Java, C, Javascript, PHP, Html, CSS, C++, and a few random languages like Scheme and Scratch.  I also have experience with the three major content management systems and a number of wiki formats.  During the day I'm a sys admin and product/service manager at Harvard Business School.</a></p>
+	    		<br>
+	    		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+	    		<p>My primary machine is a Macbook Pro. My gaming machine is a Lenovo Windows box and my house has no less than 10 computers at any time.  I also keep a Linux Live boot USB around.  I'm pretty fluid when it comes to platform.</p>
+	    		<br>
+	    		<h2>Anything else we should know?</h2>
+	    		<p>I'm here to help!  Reach out if you start falling behind on Slack, Piazza or Email.  We'll figure it out!</p>
+	    		<br>
+    		</div>
+	    </div>  <!-- close row -->
+	</div>
+	</section>
+
+	<!-- Portfolio Link Section -->
+	<section id="links" data-speed="4" data-type="background">
+		<div class="container">
+			<div class="row-fluid">
+		        <div class="span3 well">
+		        <!-- Links Anchor -->
+    			<a name="links"></a>
+		          <h2>P2</h2>
+		          <p>Here is where I will link out to my demo of P2</p>
+		          <p><a class="btn btn-inverse" href="http://p2.jenniwhitman.me">View demo &raquo;</a></p>
+		          <p><a class="btn btn-inverse" href="#">View repo &raquo;</a></p>
+		        </div><!-- /.span3 -->
+		        <div class="span3 well">
+		          <h2>P3</h2>
+		          <p>Here is where I will link out my demo of P3</p>
+		          <p><a class="btn btn-inverse" href="http://p3.jenniwhitman.me">View demo &raquo;</a></p>
+		          <p><a class="btn btn-inverse" href="#">View repo &raquo;</a></p>
+		        </div><!-- /.span3 -->
+		        <div class="span3 well">
+		          <h2>P4</h2>
+		          <p>Here is where I will link out my demo of P4</p>
+		          <p><a class="btn btn-inverse" href="http://p4.jenniwhitman.me">View demo &raquo;</a></p>
+		          <p><a class="btn btn-inverse" href="#">View repo &raquo;</a></p>
+		        </div><!-- /.span3 -->
+		        <div class="span3 well">
+		          <h2>e42 demo</h2>
+		          <p>Here is my e42 project - science writing!</p>
+		          <p><a class="btn btn-inverse" href="http://jenniwhitman.me/e42/e42.html">View demo &raquo;</a></p>
+		          <p><a class="btn btn-inverse" href="https://github.com/JenniWhitman/e42">View repo &raquo;</a></p>
+		        </div><!-- /.span3 -->
+	    	</div>
+	    </div>
+	</section>
+
+	<!-- Contact Section -->
+	<section id="contact" data-speed="2" data-type="background">
+		<div class="container">
+			<div class="page-header">
+				<a name="contact"></a>
+				<h1>Need to get a hold of me? <small>That's easy...</small></h1>
+			</div>
+			<div class="row-fluid">
+		        <div class="span4">
+		          <h2>Need help?</h2>
+		          <p>There are a number of tools.  Check out <a href="http://dwa15.com">dwa15.com</a> for more info.  Join the Piazza and post,Find me on Slack, come to Wednesday night sections, or <a href=<"mailto:jenni@dwa15.com?Subject=Website%20Help">email me</a>.</p>
+		        </div><!-- /.span4 -->
+	    	</div>
+	    </div>
+	</section>
+
+
+<!-- Include jQuery and bootstrap JS plugins -->
+<script type="text/javascript">
+  jQuery(document).ready(function($) {
+    $(".scroll").click(function(event) {
+    event.preventDefault();
+    $('html,body').animate( { scrollTop:$(this.hash).offset().top } , 1000);
+    } );
+  } );
+</script>
+<script src="includes/jquery/jquery-2.1.0.min.js"></script>
+<script src="includes/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
